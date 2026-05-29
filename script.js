3e058da1ad76bd6bf3c8bea3023e69555b14ba74
Krutant393 <deshmukhkrutant@gmail.com>
2026-05-29 17:42:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -158,12 +158,12 @@ function saveToGoogleSheets(name, sgpa, subjects, cgpa) {
   });
 
 
-  const sgpa = (weightedPoints/totalCredits).toFixed(2);
-const sem1sgpa=document.getElementById("sem1-sgpa").value.trim();
+const sgpa = (weightedPoints / totalCredits).toFixed(2);
 
+const sem1sgpa =Number(document.getElementById("sem1-sgpa").value.trim() || 0);
 
-const cgpa=(sgpa*21+sem1sgpa*19)/40).toFixed(2);
-        
+const cgpa =((Number(sgpa) * 21 + sem1sgpa * 19) / 40).toFixed(2);
+
 console.log(cgpa);
 
 
