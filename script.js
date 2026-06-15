1ec95de446bee227f53242c7eeebfbe50f4956f1
Krutant393 <deshmukhkrutant@gmail.com>
2026-06-15 15:55:12 +0530

    Refactor functions and improve SGPA calculation

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function gradeFromGP(gp) {
 
 
 
-function saveToGoogleSheets(name, sgpa, subjects, cgpa,sem1sgpa {
+function saveToGoogleSheets(name, sgpa, subjects, cgpa,sem1sgpa) {
   fetch("https://script.google.com/macros/s/AKfycbwzbOnce70J7_0GPkh6HzgYETGc4-C2b-sCBOtpH8fMZPvRvJg0k1RxEJNYi2Lm9-8SSw/exec", {
     method: "POST",
     mode: "no-cors",
